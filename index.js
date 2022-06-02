babe34cbc4072fe144fd8b6cc6810b9c879fb22e
QuentinCode <quentin.gibouin@gmail.com>
2022-06-03 01:02:58 +0200

    Page d'accueil Desktop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btnMenu = document.querySelector('.btn-rond-menu');
 const nav = document.querySelector('.nav-gauche');
-const allItemNav = document.querySelector('.nav-menu-item');
+const allItemNav = document.querySelectorAll('.nav-menu-item');
 const ligne = document.querySelector('.cont-ligne');
 
 btnMenu.addEventListener('click', () => {
