08758ccfc7fe7c193e4519cfa0d5f321fb2c74fc
QuentinCode <quentin.gibouin@gmail.com>
2022-06-04 14:44:45 +0200

    Accueil fait !

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ if(window.matchMedia('(max-width: 1300px)')) {
         })
     })
 }
+
+// animation ecriture 
+
+const txtAnim = document.querySelector('.txt-animation');
+
+let typewriter = new Typewriter(txtAnim, {
+    loop: false,
+    deleteSpeed: 20
+})
+
+typewriter
+.pauseFor(1800)
+.changeDelay(20)
+.typeString('Moi c\'est Quentin Gibouin')
+.pauseFor(300)
+.typeString('<strong>, Développeur Front-End</strong> !')
+.pauseFor(1000)
+.deleteChars(11)
+.typeString('<span style="color: #27ae60;"> HTML</span> !')
+.pauseFor(1000)
+.deleteChars(6)
+.typeString('<span style="color: #EA39ff;"> CSS</span> !')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color: #ff6910;"> JavaScript</span> !')
+.pauseFor(1000)
+.deleteChars(12)
+.typeString('<span style="color: midnightblue;"> React</span> !')
+.start()
